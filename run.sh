3ec660623d3a048014a3ac79ad93b92789aa4d2d
doondu <rabbit7183@gmail.com>
2025-06-23 14:08:02 +0900

    Update run script with annotation

--- run.sh
+++ run.sh
@@ -6,3 +6,7 @@ QEMU=qemu-system-riscv32
 
 #start QEMU
 $QEMU -machine virt -bios fw_jump.bin -nographic -serial mon:stdio --no-reboot
+# fw_jump.bin: BIOS binary file
+# -nographic: Base on console type
+# --no-reboot: Not use auto reboot
+# -machine virt: Use RISC-V model which can make virtual environment
