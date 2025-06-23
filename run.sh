42fa139c4d8787b3ee3f837166275b1972a532a8
doondu <rabbit7183@gmail.com>
2025-06-23 14:40:09 +0900

    Update Boot file

--- run.sh
+++ run.sh
@@ -4,8 +4,12 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+# Find bin file direct
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+FW_PATH="$SCRIPT_DIR/opensbi-riscv32-generic-fw_dynamic.bin"
+
 #start QEMU
-$QEMU -machine virt -bios fw_jump.bin -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios "$FW_PATH" -serial mon:stdio --no-reboot
 # fw_jump.bin: BIOS binary file
 # -nographic: Base on console type
 # --no-reboot: Not use auto reboot
